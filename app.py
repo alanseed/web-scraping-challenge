99dbae9bdf963b9ec059686ff602631855b35ddd
alan.seed <alan.seed@gmail.com>
2021-07-28 19:17:21 +1000

    WIP initial app commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func 
+from sqlalchemy import inspect 
+
+from flask import Flask, json, jsonify
+import scrape_mars 
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return () 
+
+@app.route("/scrape")
+def scrape(): 
+    data = scrape_mars.scrape() 
+    keys = data.keys() 
+    for key in keys: 
+        print(f"Key = {key}")
+        print(data[key])
+    return ()
