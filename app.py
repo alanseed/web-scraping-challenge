881a5ca89efd2af60d5bb8424f5b4b727b2d5570
alan.seed <alan.seed@gmail.com>
2021-07-31 17:29:18 +1000

    wip added the mongodb to scrape

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-from sqlalchemy import func 
-from sqlalchemy import inspect 
+import pymongo
 
 from flask import Flask, json, jsonify
 import scrape_mars 
@@ -15,12 +9,22 @@ import scrape_mars
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return () 
+    return ("hello world") 
 
 @app.route("/scrape")
 def scrape(): 
+    #scrape the data and add the time of the scrape 
     data = scrape_mars.scrape() 
-    return jsonify(data)
+    data["date"] = dt.datetime.utcnow()
+    
+    #add the dictionary to the mongo database 
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+    db = client['scrape_database']
+    collection = db['scrape_collection'] 
+    collection.insert_one(data)
+
+    return "1"
 
 if __name__ == '__main__':
     app.run(debug=True)
